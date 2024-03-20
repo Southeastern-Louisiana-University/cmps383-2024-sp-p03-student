747fd48ba678348e55914b70b91563f3e4d0a2a8
mvid <mvid@envoc.com>
2024-03-20 17:52:07 -0500

    feat: basic login

--- Selu383.SP24.Web/src/routes/home/Login.tsx
+++ Selu383.SP24.Web/src/routes/home/Login.tsx
@@ -4,33 +4,50 @@ import { FormEvent, useState } from "react";
 export default function Login() {
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
-
-  const { loading, error, post } = useFetch("/api/authentication/login", {
+  const [error, setError] = useState("");
+  const { loading, post } = useFetch("/api/authentication/login", {
     method: "post",
+    onNewData: (_, x) => {
+      if (typeof x === "string") {
+        setError(x);
+      } else if (typeof x === "object") {
+        console.log("we logged in as: ");
+        console.log(x);
+        // TODO: save in context and redirect to home page
+      }
+    },
   });
 
   return (
     <form onSubmit={handleSubmit}>
+      <label htmlFor="email">Email</label>
       <input
+        id="email"
         value={userName}
         onChange={(e) => setUserName(e.target.value)}
         type="text"
         autoComplete="email"
+        placeholder="Email"
+        required
       />
+      <label htmlFor="password">Password</label>
       <input
+        id="password"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
         type="password"
         autoComplete="password"
+        required
       />
       {loading ? "Checking Login..." : null}
-      {error ? "invalid username or password" : null}
+      {error ? error : null}
       <button type="submit" disabled={loading}>
         Submit
       </button>
     </form>
   );
-  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault(); // stops the browser from causing a page refresh - more on this in the lecture
 
     if (loading) {
